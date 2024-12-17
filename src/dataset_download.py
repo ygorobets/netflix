6d4eee854c3cf60981444c9e68ac4dcb36fa0670
Yulia_Gorobets <igorobetsyulia@gmail.com>
2024-12-17 13:50:13 +0200

    Added function for downloading dataset (with deleting source directory in cache)

--- src/dataset_download.py
+++ src/dataset_download.py
@@ -2,18 +2,34 @@ import os
 import shutil
 import kagglehub
 
-path = kagglehub.dataset_download('shivamb/netflix-shows')
+
+def download_and_move_dataset(dataset_name, destination_path, expected_file):
+    """
+    Downloads a dataset from Kaggle Hub, moves the specified file to the destination,
+    and optionally removes the source directory from the cache.
+
+    Args:
+        dataset_name (str): Name of the Kaggle dataset.
+        destination_path (str): Path to the destination directory.
+        expected_file (str): Name of the file to be moved.
+    """
+
+    path = kagglehub.dataset_download(dataset_name)
+    file_to_move = os.path.join(path, expected_file)
+
+    if os.path.exists(file_to_move):
+        destination_file = os.path.join(destination_path, expected_file)
+        if os.path.exists(destination_file):
+            os.remove(destination_file)
+        shutil.move(file_to_move, destination_path)
+        print(f"File {expected_file} successfully moved to {destination_path}")
+
+        shutil.rmtree(path)
+        print("Source directory in cache removed")
+    else:
+        print(f"File {expected_file} not found in the downloaded dataset.")
+
 
 current_dir = os.getcwd()
 project_path = os.path.join(current_dir, '../data')
-
-expected_file = 'netflix_titles.csv'
-file_to_move = os.path.join(path, expected_file)
-
-if os.path.exists(os.path.join(project_path, expected_file)):
-    os.remove(os.path.join(project_path, expected_file))
-    shutil.move(file_to_move, project_path)
-    print(f'File {expected_file} successfully overwritten in {project_path}')
-else:
-    shutil.move(file_to_move, project_path)
-    print(f'File {expected_file} successfully moved to {project_path}')
+download_and_move_dataset('shivamb/netflix-shows', project_path, 'netflix_titles.csv')
